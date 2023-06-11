1407327db783a20482ef87da5dcafbaf2101bf8d
ranjeetmehta <ranjeetkumarmehta14418@gmail.com>
2023-06-11 18:20:34 +0530

    push

--- src/main/java/com/driver/controllers/CustomerController.java
+++ src/main/java/com/driver/controllers/CustomerController.java
@@ -45,4 +45,51 @@ public class CustomerController {
 	public void cancelTrip(@RequestParam Integer tripId){
 		customerService.cancelTrip(tripId);
 	}
+}package com.driver.controllers;
+
+		import java.util.List;
+
+		import org.springframework.beans.factory.annotation.Autowired;
+		import org.springframework.http.HttpStatus;
+		import org.springframework.http.ResponseEntity;
+		import org.springframework.web.bind.annotation.*;
+
+		import com.driver.model.Customer;
+		import com.driver.model.Driver;
+		import com.driver.model.TripBooking;
+		import com.driver.services.CustomerService;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@Autowired
+	CustomerService customerService;
+
+	@PostMapping("/register")
+	public ResponseEntity<Void> registerCustomer(@RequestBody Customer customer){
+		customerService.register(customer);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@DeleteMapping("/delete")
+	public void deleteCustomer(@PathVariable Integer customerId){
+		customerService.deleteCustomer(customerId);
+	}
+
+	@PostMapping("/bookTrip")
+	public ResponseEntity<TripBooking> bookTrip(@PathVariable Integer customerId, @RequestParam String fromLocation, @RequestParam String toLocation, @RequestParam Integer distanceInKm) throws Exception {
+		TripBooking bookedTrip = customerService.bookTrip(customerId, fromLocation, toLocation, distanceInKm);
+		return new ResponseEntity<TripBooking>(bookedTrip, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/complete/{tripId}")
+	public void completeTrip(@PathVariable("tripId") Integer tripId){
+		customerService.completeTrip(tripId);
+	}
+
+	@DeleteMapping("/cancelTrip")
+	public void cancelTrip(@RequestParam Integer tripId){
+		customerService.cancelTrip(tripId);
+	}
 }
--- src/main/java/com/driver/model/Cab.java
+++ src/main/java/com/driver/model/Cab.java
@@ -1,49 +1,39 @@
 package com.driver.model;
 
-import javax.persistence.*;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 @Entity
-public class Cab {
-
+public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer Id;
-    private Integer perKmRate;
-    private Boolean available;
+    private Integer adminId;
 
-    @OneToOne
-    @JoinColumn
-    Driver driver;
+    private String username;
+    private String password;
 
-    public Integer getId() {
-        return Id;
+    public Integer getAdminId() {
+        return adminId;
     }
 
-    public void setId(Integer id) {
-        Id = id;
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
     }
 
-    public Integer getPerKmRate() {
-        return perKmRate;
+    public String getUsername() {
+        return username;
     }
 
-    public void setPerKmRate(Integer perKmRate) {
-        this.perKmRate = perKmRate;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public Boolean getAvailable() {
-        return available;
+    public String getPassword() {
+        return password;
     }
 
-    public void setAvailable(Boolean available) {
-        this.available = available;
-    }
-
-    public Driver getDriver() {
-        return driver;
-    }
-
-    public void setDriver(Driver driver) {
-        this.driver = driver;
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
